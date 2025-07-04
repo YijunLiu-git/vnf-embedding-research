a17b171fa813c3c87d9dbaec98798cd014584628
YijunLiu-git <liuyijun216@gmail.com>
2025-07-04 21:17:26 +0100

    Delete project directory

--- project/rewards/__init__.py
+++
@@ -1,9 +0,0 @@
-def get_reward_function(version):
-    if version == 'v1':
-        from .reward_v1 import basic_reward
-        return basic_reward
-    elif version == 'v2_hierarchical':
-        from .reward_v2_hierarchical import hierarchical_reward
-        return hierarchical_reward
-    else:
-        raise ValueError(f"Unsupported reward version: {version}")
